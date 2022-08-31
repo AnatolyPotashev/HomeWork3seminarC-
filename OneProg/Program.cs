2c48df6596e1b452bdfa60dc558756580abd9f2c
Anatoly <up.many.money@gmail.com>
2022-08-31 21:35:59 +0300

    Restored one programm

---
+++ OneProg/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом
+
+Console.WriteLine("Введите 5-ти значное число:");
+string text = Console.ReadLine();
+char[] obrtext = text.ToCharArray();
+
+Array.Reverse(obrtext); 
+string finaltext = new string(obrtext);
+
+if (finaltext == text)
+{
+    Console.WriteLine("Палиндром");
+}
+else
+{
+    Console.WriteLine("НЕ палиндром");
+}
+
+// Гуглил, часть кода не понимал как написать, сейчас получилось, разбираюсь!
