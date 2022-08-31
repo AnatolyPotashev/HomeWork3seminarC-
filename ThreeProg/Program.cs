d534cf885de12e6fb56aae799f11f9b61afef8ae
Anatoly <up.many.money@gmail.com>
2022-08-31 21:40:04 +0300

    Restored three programm

---
+++ ThreeProg/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число:");
+double max = Convert.ToInt32(Console.ReadLine());
+double i = 1;
+
+for (i = 1; i <= max; i++)
+{
+    Console.Write(Math.Pow(i, 3)+" ");
+}
